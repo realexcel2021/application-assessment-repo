c0c99c0b2c59ee8374ff641af3d9d0a762275016
Sheriff Joe Excel <realexcel2020@gmail.com>
2024-04-10 02:14:16 +0100

    updated scaling

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -19,7 +19,7 @@ resource "aws_launch_template" "docker_lt" {
 resource "aws_autoscaling_policy" "bat" {
   name                   = "docker_lt-policy"
   scaling_adjustment     = 4
-  adjustment_type        = "ChangeInCapacity"
+  adjustment_type        = "PredictiveScaling"
   cooldown               = 300
   autoscaling_group_name = aws_autoscaling_group.docker_asg.name
   predictive_scaling_configuration {
